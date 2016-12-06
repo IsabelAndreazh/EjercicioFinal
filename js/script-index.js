172e21c630f76752c6dab48f1f9d98422e9940fd
IsabelAndreazh <isabelandreazh@gmail.com>
2016-12-06 11:35:14 -0600

    Etapa 2

--- js/script-index.js
+++ js/script-index.js
@@ -3,6 +3,13 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 		$(".icon-arrow-left-alt").hide();
+
+		function printNews(){
+ 		 	var noticia = $('#noticia');
+		 	noticia.append("NUEVAS RECETAS");
+		 	console.log(noticia);
+ 		}
+ 		printNews();
 });
 
 
